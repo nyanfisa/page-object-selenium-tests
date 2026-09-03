4ccbd38c9f8813ed8e1e65f9c4dc1e7853cb9c30
nyanfisa <anfisadonskaya@gmail.com>
2026-09-03 23:36:39 +0300

    Update test to navigate to login page via MainPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium.webdriver.common.by import By
 import time
-from .pages.base_page import BasePage
+from pages.base_page import BasePage
+from pages.main_page import MainPage
 
 
 def test_open_steam(browser):
     url = "https://store.steampowered.com/"
-    base_page = BasePage(browser, url)
-    base_page.open()
+    page = MainPage(browser, url)
+
+    page.open()
+    page.go_to_login_page()
   
-    assert "Steam" in browser.title
