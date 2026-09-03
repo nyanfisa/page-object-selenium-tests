497d5e74c996e442c89f3753ddac435eb1c3dc0c
nyanfisa <anfisadonskaya@gmail.com>
2026-09-03 20:31:42 +0300

    Project setup: Selenium, pytest, POM structure

---
+++ pages/main_page.py
@@ -0,0 +1,7 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, '#global_action_menu > a.global_action_link')
+        login_link.click()
