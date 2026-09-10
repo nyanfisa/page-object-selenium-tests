8e2addf2192dcb2239b1dd8bf44f40de3f39964a
nyanfisa <anfisadonskaya@gmail.com>
2026-09-11 01:01:17 +0300

    Add README file

--- test_main_page.py
+++ test_main_page.py
@@ -7,20 +7,6 @@ from pages.login_page import LoginPage
 from pages.register_page import RegisterPage
 
 
-# def test_open_steam(browser):
-#     url = "https://store.steampowered.com/"
-#     page = MainPage(browser, url)
-
-#     page.open()
-#     page.should_be_login_link()
-#     page.go_to_login_page()
-
-#     page.should_be_login_page()
-#     page.go_to_register_page()
-
-#     page.should_be_register_page()
-
-
 def test_open_steam(browser):
     url = "https://store.steampowered.com/"
     
@@ -29,7 +15,7 @@ def test_open_steam(browser):
     main_page.open()
     main_page.should_be_login_link()
     main_page.go_to_login_page()
-    
+
     # 2. Страница логина
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
